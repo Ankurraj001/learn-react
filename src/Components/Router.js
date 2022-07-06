01869ba9c5a1aa4b937d17079307b759ae5da02e
ankurraj <ankur.raj@xpressbees.com>
2022-07-06 12:33:58 +0530

    table added

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,12 +3,12 @@ import { useRoutes} from 'react-router-dom';
 import ReadFile from './ReadFile';
 import About from './About';
 import Data from './Data';
-import Table from './Table';
+import Tables from './Tables';
 
 
 const Router = () => {
     const routes = useRoutes([
-        { path: '/', element: <Table /> },
+        { path: '/', element: <Tables /> },
         { path: 'about', element: <About /> },
     ]);
 
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,24 +6,32 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 159, 6.0, 24, 4.0),
+  createData('Cadbury', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Eclair', 305, 3.7, 67, 4.3),
+  createData('Eclair', 356, 16.0, 49, 3.9),
 ];
 
 export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
+      <Table sx={{'td, th': {paddingX: '0px'}}} aria-label="simple table">
+      <colgroup>
+      <col style={{width:'10%'}}/>
+      <col style={{width:'10%'}}/>
+      <col style={{width:'10%'}}/>
+      <col style={{width:'10%'}}/>
+      <col style={{width:'10%'}}/>
+   </colgroup>
+        <TableHead sx={{background: 'gray'}}>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
             <TableCell align="right">Calories</TableCell>
@@ -36,10 +44,10 @@ export default function BasicTable() {
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 }, 'td, th': {paddingX: '0px', fontSize: '16px'} }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+               {row.name === 'Eclair' ? <AddLocationIcon /> : row.name}
               </TableCell>
               <TableCell align="right">{row.calories}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
